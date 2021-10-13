0d95b1f15b0d39e3787cb00f4c1bf0cbdb554b17
MariaKobryn <kobinmary2508@gmail.com>
2021-10-14 00:44:13 +0300

    first master commit

---
+++ Лаб4.1/Лаб4.1.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+	int N, i;
+	double P;
+
+	cout << "N = "; cin >> N;
+
+	P = 1;
+	i = N;
+	while (i <= 16)
+	{
+		P *= (i*N) / (i*i + N*N);
+		i++;
+	}
+	cout << P << endl;
+
+	P = 1;
+	i = N;
+	do
+	{
+		P *= (i * N) / (i * i + N * N);
+		i++;
+	} while (i <= 16);
+	cout << P << endl;
+
+	P = 1;
+	for (i = N; i <= 16; i++)
+	{
+		P *= (i * N) / (i * i + N * N);
+	}
+	cout << P << endl;
+
+	P = 1;
+	for (i = 16; i >= N; i--)
+	{
+		P *= (i * N) / (i * i + N * N);
+	}
+	cout << P << endl;
+
+	return 0;
+}
